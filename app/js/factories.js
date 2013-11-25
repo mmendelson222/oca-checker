70a8a9d99fd38532af73637622833da51db377e5
Michael Mendelson <mmendelson@gmail.com>
2013-11-25 09:48:56 -0500

    authenticating against web service (included in project)

--- app/js/factories.js
+++ app/js/factories.js
@@ -7,31 +7,45 @@
 angular.module('myApp.factories', []).
 
 //for checking the user's login
-factory('loginFactory', ['$http', 'userService', function(http, userService) {
+factory('loginFactory', ['$http', $'userService', function($http, userService) {
 	return {
         login: function(user) {
-			if (userService.isLogged) {
+			if (userService.isLogged()) {
 				alert("You're already logged in");
 				return true;
 			}
-			//for testing - log in if both are valid.
-            if (user['ptin'] && user['email']) { 
-				userService.isLogged = true;
-				alert("You're now logged in.");
-				return true; 
-			}
 			
-			alert("You are missing some information.");
-			return false;
+			var $ptin=user['ptin'];
+			var $email=user['email'];
+			
+			if (!$ptin || !$email) {
+				alert("You are missing some information.");
+				return false;
+			}
+
+			//note: this call is asynchronous.
+			$http({
+				method: 'GET', 
+				url: "/svc/login.php?ptin=" + $ptin + "&email=" + $email
+			}).
+			success(function(data, status, headers, config) {
+				userService.token = data['token'];
+				userService.ptin = $ptin;
+				alert("success");
+			}).
+			error(function(data, status, headers, config) {
+				alert("error when connecting to the web service: "+status);
+			})
         },
 
 		logout: function() {
-			userService.isLogged = false;
+			userService.token = 0;
+			userService.ptin = '';
 		}, 
 		
 		isloggedin: function() {
 			alert("consulting isloggedin function");
-			return userService.isLogged;
+			return userService.isLogged();
 		}
     };
 }])
@@ -39,14 +53,13 @@ factory('loginFactory', ['$http', 'userService', function(http, userService) {
 //for storing user information
 .factory('userService', [function() {
 	var sdo = {
-		isLogged: false,
+		isLogged: function() {
+			return sdo.token;
+		},
+		token: 0,
 		ptin: ''
 	};
 	return sdo;
-}])
-
-.factory('logoutService', [function() {
-	userService.isLogged = false;
 }]);
 
 
